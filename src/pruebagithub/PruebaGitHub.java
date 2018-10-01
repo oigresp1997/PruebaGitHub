2ad21d6f8ac4d15f9f576f1a3513d91132516c74
CicloM <CicloM@I2-PC22>
2018-10-01 11:43:44 +0200

    Actualizado código cambio linea forzado (prueba)

--- src/pruebagithub/PruebaGitHub.java
+++ src/pruebagithub/PruebaGitHub.java
@@ -15,7 +15,7 @@ public class PruebaGitHub {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Prueba GitHub");
+        System.out.println("Prueba2 GitHub");
     }
     
 }
